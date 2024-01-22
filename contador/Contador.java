0c2af1cf1c18a5232b7b1207d374b2aeedee1e13
Tamires Ferreira <taamiresferreiraa@hotmail.com>
2024-01-22 19:09:44 -0300

    Criação do desafio proposto Contador. Os outros são do módulo Fundamentos da plataforma Java. Foi criado em outro repositório e transferi para este.

---
+++ contador/Contador.java
@@ -0,0 +1,39 @@
+package contador;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parametro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+        // realizar o for para imprimir os números com base na variável contagem
+
+    }
+}
